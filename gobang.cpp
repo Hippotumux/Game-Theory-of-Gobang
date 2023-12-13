47684a41e74323de33657b82fa405e1fd20894f4
Hippotumux <st6805972@gmail.com>
2023-12-13 01:45:54 +0000

    to update gobang.cpp gobang.h

--- gobang.cpp
+++ gobang.cpp
@@ -1,4 +1,5 @@
 #include gobang.h
+#include <iostream>
 
 void ChessMap::showboard() {
     for (int board_y = 0 ; board_y <= this->length ; board_y++) {
@@ -9,7 +10,102 @@ void ChessMap::showboard() {
     }
 }
 
-void ChessMap::checkwin(int pos_x, int pos_y) {
+vector<pair<int,int>> ChessMap::get_live_die() {
+    // left to right and up to doun
+    int sum_leftright = 0, sum_leftright_live = 0, sum_updown = 0, sum_updown_live = 0;
+    for (int shift = -1 ; shift >= -5 ; shift --) {
+        if (this->chessboard[pos_x + shift][pos_y] == player) {
+            sum_leftright += 1;
+        } else if (this->chessboard[pos_x + shift][pos_y] == 0){
+            sum_leftright_live += 1;
+            break;
+        } else {
+            break;
+        }
+
+        if (this->chessboard[pos_x][pos_y + shift] == player) {
+            sum_updown += 1;
+        } else if (this->chessboard[pos_x][pos_y + shift] == 0){
+            sum_updown_live += 1;
+            break;
+        } else {
+            break;
+        }
+    }
+
+    for (int shift = 1; shift <= 5 ; shift ++) {
+        if (this->chessboard[pos_x + shift][pos_y] == player) {
+            sum_leftright += 1;
+        } else if (this->chessboard[pos_x + shift][pos_y] == 0){
+            sum_leftright_live += 1;
+            break;
+        } else {
+            break;
+        }
+        
+        if (this->chessboard[pos_x][pos_y + shift] == player) {
+            sum_updown += 1;
+        } else if (this->chessboard[pos_x][pos_y + shift] == 0){
+            sum_updown_live += 1;
+            break;
+        } else {
+            break;
+        }
+    }
+
+    // left up to left down and right down to right up 
+    int sum_leftup = 0, sum_leftup_live = 0, sum_rightup = 0, sum_rightup_live = 0;
+    for (int shift = -1 ; shift >= -5 ; shift --) {
+        if (this->chessboard[pos_x + shift][pos_y + shift] == player) {
+            sum_leftup += 1;
+        } else if (this->chessboard[pos_x + shift][pos_y + shitf] == 0){
+            sum_leftup_live += 1;
+            break;
+        } else {
+            break;
+        }
+
+        if (this->chessboard[pos_x + shift][pos_y - shift] == player) {
+            sum_rightup += 1;
+        } else if (this->chessboard[pos_x + shift][pos_y - shitf] == 0){
+            sum_rightup_live += 1;
+            break;
+        } else {
+            break;
+        }
+    }
+
+    for (int shift = 1; shift <= 5 ; shift ++) {
+        if (this->chessboard[pos_x + shift][pos_y + shift] == player) {
+            sum_leftup += 1;
+        } else if (this->chessboard[pos_x + shift][pos_y + shitf] == 0){
+            sum_leftup_live += 1;
+            break;
+        } else {
+            break;
+        }
+
+        if (this->chessboard[pos_x + shift][pos_y - shift] == player) {
+            sum_rightup += 1;
+        } else if (this->chessboard[pos_x + shift][pos_y - shitf] == 0){
+            sum_rightup_live += 1;
+            break;
+        } else {
+            break;
+        }
+    }
+
+    //return all situation
+    vector<pair<int,int>> result(4);
+    result[0] = std::make_pair(sum_leftright, sum_leftright_live);
+    result[1] = std::make_pair(sum_updown, sum_updown_live);
+    result[2] = std::make_pair(sum_leftup, sum_leftup_live);
+    result[3] = std::make_pair(sum_rightup, sum_rightup_live);
+    return result;
+}
+
+bool ChessMap::checkwin(int pos_x, int pos_y, int player) {
+    this->live_die = this->get_live_die();
     
 }
 
--- gobang.h
+++ gobang.h
@@ -1,13 +1,25 @@
 #pragma once
+#include <pair>
+#include <vector>
 
 class Chess:
 {
 public:
     // chessboard width and length
     int width = 15, length = 15;
+    
+    // player1 user, player2 computer 
+    int player;
+
+    // 0 : empty, 1 : player1, 2 : player2
     int chessboard[width+2][length+2] = {0};
+
+    // 4 direction live and die;
+    std::vector<std::pair<int,int>> live_die;
+    std::vector<std::pair<int,int>> get_live_die() {};
+
     void showboard(void) {};
-    void checkwin(int pos_x, int pos_y) {};
+    bool checkwin(int pos_x, int pos_y, int player) {};
 } 
 
 class point_chart
@@ -22,6 +34,7 @@ class user: public Chess
 {
 public:
     int choose_x, int choose_y;
+    player = 1;
 }
 
 class minmax_algorithm : public Chess
